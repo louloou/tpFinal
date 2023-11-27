04e28773feac751c1bcc7f5d4ce19664009414ad
alumno <alumno@Instituto.local>
2023-11-27 10:47:14 -0300

    Uptade

--- document.js
+++ document.js
@@ -1,12 +1,25 @@
 let = fotoMostrada = "imagen/loverS1.jpg"
 
 
-function miFuncion(){
-    let elemento= document.getElementById("foto")
+function miFuncion() {
+    let elemento = document.getElementById("foto")
     elemento.src = "imagenes/loverS2.jpg"
-
-    if(fotoMostrada == "imagenes/loverS1.jpg"){
-        imagen.src ="imagenes/loverS2.jpg";
-        fotoMostrada= "imagenes/loverS2.jpg"
+    if (fotoMostrada == "imagenes/loverS1.jpg") {
+        imagen.src = "imagenes/loverS2.jpg";
+        fotoMostrada = "imagenes/loverS2.jpg"
+    } else if (fotoMostrada == "imagenes/loverS2.jpg") {
+        imagen.src = "imagenes/loverS3.jpg";
+        fotoMostrada = "imagenes/loverS3.jpg"
+    } else if (fotoMostrada == "imagenes/loverS3.jpg"){
+        imagen.src = "imagenes/loverS4.jpg";
+        fotoMostrada = "imagenes/loverS4.jpg"
+        }else if(fotoMostrada == "imagenes/4.jpg"){ 
+            imagen.src = "imagenes/loverS1.jpg";
+            fotoMostrada = "imagenes/loverS1.jpg"
+        }
     }
-}
+
+
+   
+
+                                                                                                                                                                                                                                                                                            
